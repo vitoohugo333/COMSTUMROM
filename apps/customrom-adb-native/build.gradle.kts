067de134ebb2926417ee2e7ff2a03821d288aa3d
vitoohugo333 <hugovitorslipknot@gmail.com>
2026-08-07 22:57:32 -0300

    build: upgrade native app to AGP 9.3.1

--- apps/customrom-adb-native/build.gradle.kts
+++ apps/customrom-adb-native/build.gradle.kts
@@ -1,4 +1,3 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    kotlin("android") version "2.1.20" apply false
+    id("com.android.application") version "9.3.1" apply false
 }
